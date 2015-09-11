ccfa8c2e17569c93531a0e8a06570e19853e2fc6
Andrew T. poe <techapoe@gmail.com>
2015-09-11 16:51:42 -0400

    testing deployment

--- app/assets/javascripts/slideshow/beacon/Indicator.react.js
+++ app/assets/javascripts/slideshow/beacon/Indicator.react.js
@@ -1,37 +1,26 @@
 var React = require('react');
 
+var React = require('react');
+
 var Indicator = React.createClass({
-  render: function () {
+  _handleChange: function() {
+    event.preventDefault();
+    this.props.stopSlideShow();
+    this.props.changeSlide(this.props.indicatorNumber);
+  },
+
+  render: function() {
+    if (this.props.indicatorNumber === this.props.slide) {
+      var circle = <i className="fa fa-dot-circle-o"></i>
+    } else {
+      var circle = <i className="fa fa-circle-o" ></i>
+    }
     return (
-      <div className="testClass" />
+      <div className="slideshow__beaconIndicator" onClick={this._handleChange}>
+        {circle}
+      </div>
     )
   }
-})
+});
 
 module.exports = Indicator;
-
-
-// var React = require('react');
-//
-// var Indicator = React.createClass({
-//   _handleChange: function() {
-//     event.preventDefault();
-//     this.props.stopSlideShow();
-//     this.props.changeSlide(this.props.indicatorNumber);
-//   },
-//
-//   render: function() {
-//     if (this.props.indicatorNumber === this.props.slide) {
-//       var circle = <i className="fa fa-dot-circle-o"></i>
-//     } else {
-//       var circle = <i className="fa fa-circle-o" ></i>
-//     }
-//     return (
-//       <div className="slideshow__beaconIndicator" onClick={this._handleChange}>
-//         {circle}
-//       </div>
-//     )
-//   }
-// });
-//
-// module.exports = Indicator;
