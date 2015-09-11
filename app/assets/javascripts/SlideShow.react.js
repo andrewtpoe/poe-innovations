ecd0b65285faf796ddb797a5da89f787b158985f
Andrew T. poe <techapoe@gmail.com>
2015-09-11 16:29:52 -0400

    testing deployment

--- app/assets/javascripts/SlideShow.react.js
+++ app/assets/javascripts/SlideShow.react.js
@@ -11,6 +11,54 @@ var SlideShow = React.createClass({
     }
   },
 
+  tick: function() {
+    this._nextSlide(this.state.slide);
+  },
+
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 10000);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  _stopSlideShow: function() {
+    clearInterval(this.interval);
+  },
+
+  _displaySlide: function() {
+    return (
+      <h1>
+        {this.state.slides[this.state.slide]}
+      </h1>
+    )
+  },
+
+  _changeSlide: function(slideNumber) {
+    this.setState ({
+      slide: slideNumber
+    })
+  },
+
+  _prevSlide: function(slide) {
+    if (slide - 1 >= 0) {
+      var slideNumber = slide - 1;
+    } else {
+      var slideNumber = this.state.slides.length - 1;
+    };
+    this._changeSlide(slideNumber);
+  },
+
+  _nextSlide: function(slide) {
+    if (slide + 1 >= this.state.slides.length) {
+      var slideNumber = 0;
+    } else {
+      var slideNumber = slide + 1;
+    };
+    this._changeSlide(slideNumber);
+  },
+
   render: function() {
     return (
       <Beacon />
