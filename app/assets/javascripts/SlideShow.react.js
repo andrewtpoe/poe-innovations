8acc6f19ba6c15685535de90b9344bbdddbf8e14
Andrew T. poe <techapoe@gmail.com>
2015-09-11 15:26:32 -0400

    testing deployment

--- app/assets/javascripts/SlideShow.react.js
+++ app/assets/javascripts/SlideShow.react.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var Beacon = require('./Beacon.react.js');
+var PrevSlide = require('./PrevSlide.react.js');
+var NextSlide = require('./NextSlide.react.js');
 
 var SlideShow = React.createClass({
   render: function() {
