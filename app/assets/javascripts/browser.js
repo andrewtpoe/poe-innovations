b19d7f8ecf53705067844dd35a81190d7cde15fc
Andrew T. poe <techapoe@gmail.com>
2015-09-11 14:41:14 -0400

    testing deployement

--- app/assets/javascripts/browser.js
+++ app/assets/javascripts/browser.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var SlideShow = require('./SlideShow.react.js');
+// var SlideShow = require('./SlideShow.react.js');
 
 var browser = function() {
   var div = document.querySelector('[data-js="slide-show"]');
