99529ff1c799a9f49023cfa535137028ae251688
Andrew T. poe <techapoe@gmail.com>
2015-09-11 15:17:54 -0400

    testing deployment

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,11 +1,12 @@
 var React = require('react');
-var Slides = require('./Slides.react.js');
+// var Slides = require('./Slides.react.js');
+var test = require('./test.react.js')
 
 var components = function() {
   var div = document.querySelector('[data-js="slide-show"]');
   if (div) {
-    // React.render(<Slides />, div);
-    React.render(<div className="foo" />);
+    React.render(test, div);
+    // React.render(<div className="foo" />);
   }
 };
 
---
+++ app/assets/javascripts/test.react.js
@@ -0,0 +1,5 @@
+var React = require('react');
+
+var test = <div className="foo" />
+
+module.exports = test;
