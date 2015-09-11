9d0cf054f2d3753ee086c066bc79ed324ff17ae8
Andrew T. poe <techapoe@gmail.com>
2015-09-11 16:35:07 -0400

    testing deployment

--- app/assets/javascripts/slideshow/Main.react.js
+++ app/assets/javascripts/slideshow/Main.react.js
@@ -60,10 +60,17 @@ var SlideShow = React.createClass({
   },
 
   render: function() {
+    var insert = this._displaySlide(this.state.slide);
     return (
-      <Beacon />
+      <div className="slideshow">
+        <Beacon slide={this.state.slide} slides={this.state.slides} changeSlide={this._changeSlide} stopSlideShow={this._stopSlideShow} />
+        <PrevSlide slide={this.state.slide} slides={this.state.slides} prevSlide={this._prevSlide} stopSlideShow={this._stopSlideShow} />
+        { insert }
+        <NextSlide slide={this.state.slide} slides={this.state.slides} nextSlide={this._nextSlide} stopSlideShow={this._stopSlideShow} />
+      </div>
     )
   }
+
 });
 
 module.exports = SlideShow;
