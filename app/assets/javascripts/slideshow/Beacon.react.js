159335e82e9e9e447d20042aee7630da95b7f015
Andrew T. poe <techapoe@gmail.com>
2015-09-11 16:41:20 -0400

    testing deployment

--- app/assets/javascripts/slideshow/Beacon.react.js
+++ app/assets/javascripts/slideshow/Beacon.react.js
@@ -3,16 +3,16 @@ var Indicator = require('./beacon/Indicator.react.js');
 
 var Beacon = React.createClass({
 
-  _buildBeacons() {
-    return this.props.slides.map(function(slideNumber) {
-      return <Indicator slide={this.props.slide} indicatorNumber={slideNumber} key={slideNumber} changeSlide={this.props.changeSlide} stopSlideShow={this.props.stopSlideShow}/>
-    }.bind(this))
-  },
+  // _buildBeacons() {
+  //   return this.props.slides.map(function(slideNumber) {
+  //     return <Indicator slide={this.props.slide} indicatorNumber={slideNumber} key={slideNumber} changeSlide={this.props.changeSlide} stopSlideShow={this.props.stopSlideShow}/>
+  //   }.bind(this))
+  // },
 
   render: function() {
     return (
-      <div className="slideshow__beacon">
-        {this._buildBeacons()}
+      <div className="slideshow__beacon" >
+        <Indicator />
       </div>
     )
   }
