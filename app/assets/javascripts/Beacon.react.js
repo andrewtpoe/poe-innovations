fd3f2e8be3bc1ba7ee0946aa22fb0fc3f415ab1d
Andrew T. poe <techapoe@gmail.com>
2015-09-11 15:32:49 -0400

    testing deployment

--- app/assets/javascripts/Beacon.react.js
+++ app/assets/javascripts/Beacon.react.js
@@ -1,21 +1,21 @@
-var React = require('react');
+// var React = require('react');
 // var Indicator = require('./beacon/Indicator.react.js');
-
-var Beacon = React.createClass({
-
-  _buildBeacons() {
-    return this.props.slides.map(function(slideNumber) {
-      return <Indicator slide={this.props.slide} indicatorNumber={slideNumber} key={slideNumber} changeSlide={this.props.changeSlide} stopSlideShow={this.props.stopSlideShow}/>
-    }.bind(this))
-  },
-
-  render: function() {
-    return (
-      <div className="slideshow__beacon">
-        {this._buildBeacons()}
-      </div>
-    )
-  }
-});
-
-module.exports = Beacon;
+//
+// var Beacon = React.createClass({
+//
+//   _buildBeacons() {
+//     return this.props.slides.map(function(slideNumber) {
+//       return <Indicator slide={this.props.slide} indicatorNumber={slideNumber} key={slideNumber} changeSlide={this.props.changeSlide} stopSlideShow={this.props.stopSlideShow}/>
+//     }.bind(this))
+//   },
+//
+//   render: function() {
+//     return (
+//       <div className="slideshow__beacon">
+//         {this._buildBeacons()}
+//       </div>
+//     )
+//   }
+// });
+//
+// module.exports = Beacon;
--- app/assets/javascripts/beacon/Indicator.react.js
+++ app/assets/javascripts/beacon/Indicator.react.js
@@ -1,25 +1,24 @@
-// Libraries
-var React = require('react');
-
-var Indicator = React.createClass({
-  _handleChange: function() {
-    event.preventDefault();
-    this.props.stopSlideShow();
-    this.props.changeSlide(this.props.indicatorNumber);
-  },
-
-  render: function() {
-    if (this.props.indicatorNumber === this.props.slide) {
-      var circle = <i className="fa fa-dot-circle-o"></i>
-    } else {
-      var circle = <i className="fa fa-circle-o" ></i>
-    }
-    return (
-      <div className="slideshow__beaconIndicator" onClick={this._handleChange}>
-        {circle}
-      </div>
-    )
-  }
-});
-
-module.exports = Indicator;
+// var React = require('react');
+//
+// var Indicator = React.createClass({
+//   _handleChange: function() {
+//     event.preventDefault();
+//     this.props.stopSlideShow();
+//     this.props.changeSlide(this.props.indicatorNumber);
+//   },
+//
+//   render: function() {
+//     if (this.props.indicatorNumber === this.props.slide) {
+//       var circle = <i className="fa fa-dot-circle-o"></i>
+//     } else {
+//       var circle = <i className="fa fa-circle-o" ></i>
+//     }
+//     return (
+//       <div className="slideshow__beaconIndicator" onClick={this._handleChange}>
+//         {circle}
+//       </div>
+//     )
+//   }
+// });
+//
+// module.exports = Indicator;
