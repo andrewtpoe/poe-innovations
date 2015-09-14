bcd398dec3024d6a19afee974e0ac121eedf4053
Andrew T. poe <techapoe@gmail.com>
2015-09-14 16:45:48 -0400

    style improvements & reduced home screen content

--- app/assets/javascripts/slideshow/Slide.react.js
+++ app/assets/javascripts/slideshow/Slide.react.js
@@ -5,12 +5,15 @@ var slide = React.createClass({
   _slide0: function() {
     return (
       <div className={this.props.fade} >
-        <div className="slide__intro" >
+        <div className="slide" >
           <h1>
-            Web Developers in Charleston, SC
+            Full Stack Web Development
           </h1>
           <h2>
-            Specializing in Ruby, Rails, and React.js
+            specializing in Ruby, Rails, and React.js
+          </h2>
+          <h2>
+            located in beautiful Charleston, SC
           </h2>
         </div>
       </div>
@@ -21,25 +24,17 @@ var slide = React.createClass({
     return (
       <div className={this.props.fade} >
         <div className="slide" >
-          <div className="slide__content" >
-            <h1 className="slide__title">
-              Our Services
-            </h1>
-            <div className="slide__text">
-              <h2>SaaS Development</h2>
-              <p>
-                SaaS, or software as a service, is our primary focus. At POE Innovations, we are passionate about developing amazing tools that solve the daily problems of professionals and entrepreneurs.</p>
-              <p><a href="mailto:poeinnovations@gmail.com?subject=My%20profession%20needs%20a%20new%20application">Tell us</a> what new software tools would make a difference in your daily work. If we develop an application to solve a problem you tell us about there will be no cost to you. We'll even give you a lifetime membership totally free!
-              </p>
-              <h2>Custom Web Development</h2>
-              <p>
-                Whether you need a new site built from the ground up or just a new feature on an existing site, we are happy to help. Just tell us about your needs and let's build something amazing together.
-              </p>
-              <p>
-                <a href="mailto:poeinnovations@gmail.com?subject=I would like a quote on your web development services">Contact us for a free quote!</a>
-              </p>
-            </div>
-          </div>
+          <h1 >
+            Our Services:
+          </h1>
+          <ul>
+            <li>
+              SaaS Development
+            </li>
+            <li>
+              Web Development
+            </li>
+          </ul>
         </div>
       </div>
     );
@@ -49,28 +44,16 @@ var slide = React.createClass({
     return (
       <div className={this.props.fade} >
         <div className="slide" >
-          <h1 className="slide__title" >
-            Primary Technologies
+          <h1 >
+            Primary Technologies:
           </h1>
-          <div className="slide__text" >
-            <p>
-              We pride ourselves in using a wide array of the latest technologies in as we build our mobile friendly projects. Our primary expertise is with the following:
-            </p>
-            <ul>
-              <li>Ruby on Rails</li>
-              <li>React.js</li>
-              <li>PostgreSQL</li>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>Responsive Design</li>
-            </ul>
-            <p>
-              We are familiar with many other technologies as well, plus we are always eager to learn new things so we can better serve you.
-            </p>
-            <p>
-              <a href="mailto:poeinnovations@gmail.com?subject=Let's talk tech!">Tell us what you use!</a>
-            </p>
-          </div>
+          <ul>
+            <li>Ruby on Rails</li>
+            <li>PostgreSQL</li>
+            <li>React.js</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+          </ul>
         </div>
       </div>
     );
@@ -80,16 +63,11 @@ var slide = React.createClass({
     return (
       <div className={this.props.fade} >
         <div className="slide" >
-          <h1 className="slide__title" >
-            Our Team
+          <h1>
+            Meet the CEO
           </h1>
-          <div className="slide__bio" >
-            <img src='https://avatars2.githubusercontent.com/u/11183459?v=3&s=500' className="slide__bioPic" ></img>
-            <h2 className="slide__bioTitle">Andrew T. Poe</h2>
-            <div className="slide__bioText">
-              <p>Andrew T. Poe is the CEO and Lead Developer for POE Innovations, LLC. He began his career as an automotive technician but since discovered a passion for programming. After teaching himself Python for 2 years he went through an immersive web development course at The Iron Yard where he was mentored into a professional ready to run this business. Andrew is most proficient with Ruby, Rails, and React.js, but is familiar with a wide array of other modern development techniques and technologies.</p>
-            </div>
-          </div>
+          <img src='https://avatars2.githubusercontent.com/u/11183459?v=3&s=500' className="slide__bioPic" ></img>
+          <h2>Andrew T. Poe</h2>
         </div>
       </div>
     );
@@ -119,3 +97,49 @@ var slide = React.createClass({
 });
 
 module.exports = slide;
+
+
+// <h1 className="slide__title">
+//   Our Services
+// </h1>
+// <div className="slide__text">
+//   <h2>SaaS Development</h2>
+//   <p>
+//     SaaS, or software as a service, is our primary focus. At POE Innovations, we are passionate about developing amazing tools that solve the daily problems of professionals and entrepreneurs.</p>
+//   <p><a href="mailto:poeinnovations@gmail.com?subject=My%20profession%20needs%20a%20new%20application">Tell us</a> what new software tools would make a difference in your daily work. If we develop an application to solve a problem you tell us about there will be no cost to you. We'll even give you a lifetime membership totally free!
+//   </p>
+//   <h2>Custom Web Development</h2>
+//   <p>
+//     Whether you need a new site built from the ground up or just a new feature on an existing site, we are happy to help. Just tell us about your needs and let's build something amazing together.
+//   </p>
+//   <p>
+//     <a href="mailto:poeinnovations@gmail.com?subject=I would like a quote on your web development services">Contact us for a free quote!</a>
+//   </p>
+// </div>
+
+// <h1 className="slide__title" >
+//   Primary Technologies
+// </h1>
+// <div className="slide__text" >
+//   <p>
+//     We pride ourselves in using a wide array of the latest technologies in as we build our mobile friendly projects. Our primary expertise is with the following:
+//   </p>
+//   <ul>
+//     <li>Ruby on Rails</li>
+//     <li>React.js</li>
+//     <li>PostgreSQL</li>
+//     <li>HTML5</li>
+//     <li>CSS3</li>
+//     <li>Responsive Design</li>
+//   </ul>
+//   <p>
+//     We are familiar with many other technologies as well, plus we are always eager to learn new things so we can better serve you.
+//   </p>
+//   <p>
+//     <a href="mailto:poeinnovations@gmail.com?subject=Let's talk tech!">Tell us what you use!</a>
+//   </p>
+// </div>
+
+// <div className="slide__bioText">
+//   <p>Andrew T. Poe is the CEO and Lead Developer for POE Innovations, LLC. He began his career as an automotive technician but since discovered a passion for programming. After teaching himself Python for 2 years he went through an immersive web development course at The Iron Yard where he was mentored into a professional ready to run this business. Andrew is most proficient with Ruby, Rails, and React.js, but is familiar with a wide array of other modern development techniques and technologies.</p>
+// </div>
